e30d2ad0e44b50610473dc26a8249da94f4a11e3
Shu YAMANI <mail@shuymn.me>
2026-03-17 17:48:12 +0900

    feat(cmd): migrate from singlechecker to checker

--- cmd/ptrstruct/main.go
+++ cmd/ptrstruct/main.go
@@ -1,11 +1,77 @@
 package main
 
 import (
-	"golang.org/x/tools/go/analysis/singlechecker"
+	"fmt"
+	"os"
+
+	"golang.org/x/tools/go/analysis"
+	"golang.org/x/tools/go/analysis/checker"
+	"golang.org/x/tools/go/packages"
 
 	"github.com/shuymn/ptrstruct"
 )
 
+// Exit code 3 indicates that analysis succeeded but reported diagnostics.
+// This follows the convention established by singlechecker/multichecker.
+const exitDiagnostics = 3
+
 func main() {
-	singlechecker.Main(ptrstruct.Analyzer)
+	a := ptrstruct.NewAnalyzer()
+	a.Flags.Usage = func() { printUsage(a) }
+
+	if err := a.Flags.Parse(os.Args[1:]); err != nil {
+		os.Exit(2)
+	}
+
+	patterns := a.Flags.Args()
+	if len(patterns) == 0 {
+		patterns = []string{"."}
+	}
+
+	pkgs, err := packages.Load(&packages.Config{
+		Mode:  packages.LoadAllSyntax,
+		Tests: true,
+	}, patterns...)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+
+	if packages.PrintErrors(pkgs) > 0 {
+		os.Exit(1)
+	}
+
+	graph, err := checker.Analyze([]*analysis.Analyzer{a}, pkgs, nil)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+
+	if err := graph.PrintText(os.Stderr, -1); err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+
+	exitCode := 0
+	for _, root := range graph.Roots {
+		if root.Err != nil {
+			exitCode = 1
+			continue
+		}
+		if len(root.Diagnostics) > 0 {
+			exitCode = exitDiagnostics
+		}
+	}
+
+	if exitCode != 0 {
+		os.Exit(exitCode)
+	}
+}
+
+func printUsage(a *analysis.Analyzer) {
+	fmt.Fprintf(os.Stderr, "%s: %s\n\n", a.Name, a.Doc)
+	fmt.Fprintln(os.Stderr, "Usage: ptrstruct [-flag] [package]")
+	fmt.Fprintln(os.Stderr)
+	fmt.Fprintln(os.Stderr, "Flags:")
+	a.Flags.PrintDefaults()
 }
